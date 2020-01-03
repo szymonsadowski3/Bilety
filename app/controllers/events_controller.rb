e993921b4814665e46b2ff2dd5da455e20bc6d56
SG0302219 <szymon.sadowski@sabre.com>
2020-01-03 22:10:34 +0100

    - lab 2 instruction

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show]
+  before_action :check_logged_in, :only => [:new, :create]
 
   def index
     @events = Event.all
@@ -24,8 +25,15 @@ class EventsController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def check_logged_in
+    authenticate_or_request_with_http_basic("Ads") do |username, password|
+      username == "admin" && password == "admin"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'events/index'
-
-  get 'events/new'
-  post 'events/new' => 'events#create'
-
-  get 'events/create'
+  # get 'events/index'
+  # get 'events/new'
+  # post 'events/new' => 'events#create'
+  # get 'events/create'
+  # get 'events/:id' => 'events#show'
 
   resources :tickets
-  root :to => "tickets#index"
-
-  get 'events/:id' => 'events#show'
+  resources :events, :only => [:index, :new, :create, :show]
+  # root :to => "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
