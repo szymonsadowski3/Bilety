45b5e7f75a6f23803d7c63dad7305b23502531ca
SG0302219 <szymon.sadowski@sabre.com>
2020-01-04 18:06:14 +0100

    - started impl custom validators

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,24 @@
+class PastDateValidator < ActiveModel::Validator
+  def validate(record)
+    if record.event_date < Date.today
+      record.errors.add('Data wydarzenia', 'nie może być z przeszłości')
+    end
+  end
+end
+
+class PriceRangeValidator < ActiveModel::Validator
+  def validate(record)
+    if record.price_low > record.price_high
+      record.errors.add('Cena maksymalna', 'nie może być mniejsza niż cena minimalna')
+    end
+  end
+end
+
 class Event < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with PastDateValidator
+  validates_with PriceRangeValidator
+
   validates :artist, :presence => true
   validates :price_low, :presence => true, numericality: true
   validates :price_high, :presence => true, numericality: true
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Bilety
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.encoding = "utf-8"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
