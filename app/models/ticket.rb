d34bd77ab2a9984a5d152cd30acd1579dfa511d9
SG0302219 <szymon.sadowski@sabre.com>
2020-01-04 18:15:09 +0100

    - correctly working validators

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,15 @@
+class PriceValidator < ActiveModel::Validator
+  def validate(record)
+    if !((record.event.price_low <= record.price) && (record.event.price_high >= record.price))
+      record.errors.add('Cena', 'nie mieści się w przedziale cenowym wydarzenia')
+    end
+  end
+end
+
 class Ticket < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with PriceValidator
+
   validates :name, :presence => true, :length => { :minimum => 6 }
   validates :email_address, :presence => true
   validates :price, :presence => true
